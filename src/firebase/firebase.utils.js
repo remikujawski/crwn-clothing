c6ccb8bd039759270ce95e698fc180860fbdb95a
remikujawski <remi.kujawski@gmail.com>
2022-03-17 15:09:33 +0100

    created user and cart sagas for sign-in, sign-up and sign-in-withGoogle

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -37,14 +37,22 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     }
 
     return userRef;
-}
+};
+
+export const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const unsubscribe = auth.onAuthStateChanged(userAuth => {
+            unsubscribe();
+            resolve(userAuth);
+        }, reject);
+    });
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export const addCollectionEndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef  = firestore.collection(collectionKey);
-    //console.log()
 
     const batch = firestore.batch();
     objectsToAdd.forEach(obj => {
@@ -57,7 +65,7 @@ export const addCollectionEndDocuments = async (collectionKey, objectsToAdd) => 
 
 export const convertCollectionsSnapshotToMap = (collections) => {
     const transformedCollection = collections.docs.map(doc => {
-        const {title, items} =doc.data();
+        const {title, items} = doc.data();
 
         return{
             routeName:encodeURI(title.toLowerCase()),
@@ -73,9 +81,8 @@ export const convertCollectionsSnapshotToMap = (collections) => {
     }, {});
 };
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
-
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+import {fetchCollectionsStart} from "./shop/shop.sagas";
 
 import rootReducer from './root-reducer';
+import rootSaga from "./root-saga";
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
@@ -12,4 +17,8 @@ if(process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleware.run(rootSaga);
+
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
